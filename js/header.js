6eab29baf4e69dec657c034217f154d4e5f87fac
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-02-05 11:59:59 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,1 +1,15 @@
-h
+getComputedStyle(element).getPropertyValue('--color-font-general');
+function setHeaderShownVar() {
+	document
+		.documentElement
+		.style
+		.setProperty(
+			'--js-header-shown',
+			window.innerWidth > 991 ?
+				true :
+				false
+		);
+}
+window.addEventListener("resize", () => {
+	setHeaderShownVar()
+});
