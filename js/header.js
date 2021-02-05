71c7ddad4366711bf126c65020eb7701fb8884f0
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-02-05 12:34:27 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -8,7 +8,16 @@ function setHeaderShownVar() {
 				"flex" :
 				"none"
 		);
-}
+	let navdividers = document.getElementsByClassName("nav-divider")
+	for (let i = 0; i < navdividers.length; i++) {
+		let children = navdividers[i].getElementsByTagName("a")
+		for (let i = 0; i < children.length; i++) {
+			children.animation = window.innerWidth > 991 ?
+				"slide-in-left 0.6s cubic-bezier(.25,.46,.45,.94)" :
+				"none";
+		};
+	};
+};
 setHeaderShownVar();
 window.addEventListener("resize", () => {
 	setHeaderShownVar();
