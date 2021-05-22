ac342ff6680d0a5826887b41cc8661aca9709616
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-05-22 00:20:19 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,38 +1,4 @@
-/*const urlParams = new URLSearchParams(window.location.search);
-let typed = "",
-    options = {
-        "noValue": []
-    },
-    params = urlParams.toString().split("&");
-params.forEach(param => {
-    if (param.indexOf("=") !== -1) {
-        let splitted = param.split("="),
-            key = splitted[0],
-            value = splitted[1];
-        if (!key in options) return;
-        options[key] = value; 
-    } else options.noValue.push(param);
-})
-window
-    .addEventListener(
-        "load",
-        () => {
-            document
-                .getElementsByClassName("ntk")[0]
-                .addEventListener(
-                    "contextmenu",
-                    (e) =>
-                    {
-                        e.preventDefault();
-                        return false;
-                });
-            require("js/header.js");
-});
-function clearPage() {
-    while (document.firstChild) {
-        document.removeChild(document.firstChild);
-    }
-}
+
 function keypress(event) {
     if (String(event.key).toLowerCase() == "a"
     || String(event.key).toLowerCase() == "y"
@@ -43,12 +9,3 @@ function keypress(event) {
     if (typed.indexOf("ayumi") !== -1)
         window.location = `http://samidb.xyz/AyumiIsDead`;
 };
-function toniKensa() {
-    document
-        .getElementsByClassName("ntk")[0].style
-        .setProperty("display", "none");
-    document
-        .getElementsByClassName("toniKensa")[0].style
-        .setProperty("display", "block");
-}
-*/
